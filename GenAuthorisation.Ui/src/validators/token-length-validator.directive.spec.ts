5737ebdd2cdf8681462cb89e30142c7c6629f3fe
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-09 16:47:55 +1100

    Add models, services and validators

---
+++ GenAuthorisation.Ui/src/validators/token-length-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TokenLengthValidatorDirective } from './token-length-validator.directive';
+
+describe('TokenLengthValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TokenLengthValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
